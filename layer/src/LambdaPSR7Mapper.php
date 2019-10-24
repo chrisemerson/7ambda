2e4e69a27033cc6b0f3b9e2d8b240b2a7750a7dc
Chris Emerson <Chris.Emerson@bjss.com>
2019-10-24 17:27:48 +0100

    Request mapping from Lamda API Gateway

--- layer/src/LambdaPSR7Mapper.php
+++ layer/src/LambdaPSR7Mapper.php
@@ -5,6 +5,7 @@ namespace CEmerson\Sevenambda\Layer;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\ServerRequest;
+use Zend\Diactoros\Uri;
 
 class LambdaPSR7Mapper
 {
@@ -13,10 +14,21 @@ class LambdaPSR7Mapper
 
     public function mapLambdaRequestToPSR7ServerRequest($eventPayload, $data): ServerRequestInterface
     {
-        $this->eventPayload = $eventPayload;
+        $this->eventPayload = json_decode($eventPayload);
         $this->data = $data;
 
-        return new ServerRequest();
+        $uri = new Uri(
+            $this->eventPayload->requestContext->domainName . $this->eventPayload->requestContext->path
+        );
+
+        $serverRequest = new ServerRequest(
+            [],
+            [],
+            $uri,
+            $this->eventPayload->httpMethod
+        );
+
+        return $serverRequest;
     }
 
     public function mapPSR7ResponseToLambdaResponse(ResponseInterface $response): string
